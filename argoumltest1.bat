6754d46fbb5dfea5488d3f19a6ff51c56dfc08ef
alextazin <32340201+alextazin@users.noreply.github.com>
2018-05-26 18:48:29 -0400

    Update argoumltest1.bat

--- argoumltest1.bat
+++ argoumltest1.bat
@@ -1,2 +1,2 @@
-java org.apache.xalan.xslt.Process -in proj10.xmi -xsl OWLfromUML.xsl -out outputfile.xml
+java org.apache.xalan.xslt.Process -in proj8.xmi -xsl OWLfromUML.xsl -out outputfile.xml
 pause
